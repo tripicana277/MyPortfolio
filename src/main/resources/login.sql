a755b15caf4b3f556b2801810a5d0ddcc4a2992c
tripicana277 <tropicana277@hotmail.co.jp>
2024-10-15 18:10:52 +0900

    Test

--- src/main/resources/login.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `login` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,41 @@
+CREATE TABLE `login` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `recipemain` (
+  `RECIPENAME` varchar(255) NOT NULL,
+  `FILENAME` varchar(255) DEFAULT NULL,
+  `COMMENT` varchar(255) DEFAULT NULL,
+  `NUMBER` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`RECIPENAME`)
+);
+
+CREATE TABLE `material` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `RECIPENAME` varchar(255) NOT NULL,
+  `MATERIAL` varchar(255) NOT NULL,
+  `QUANTITY` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `fk_recipe_material` (`RECIPENAME`),
+  CONSTRAINT `fk_recipe_material` FOREIGN KEY (`RECIPENAME`) REFERENCES `recipemain` (`RECIPENAME`) ON DELETE CASCADE
+);
+
+CREATE TABLE `howtomake` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `RECIPENAME` varchar(255) NOT NULL,
+  `FILENAME2` varchar(255) NOT NULL,
+  `HOWTOMAKE` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `fk_recipe_howtomake` (`RECIPENAME`),
+  CONSTRAINT `fk_recipe_howtomake` FOREIGN KEY (`RECIPENAME`) REFERENCES `recipemain` (`RECIPENAME`) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS `income` (
+  `INCOME_DATE` varchar(255) NOT NULL,
+  `INCOME_NAME` varchar(255) NOT NULL,
+  `INCOME_COUNT` int NOT NULL,
+  PRIMARY KEY (`INCOME_NAME`)
+);
